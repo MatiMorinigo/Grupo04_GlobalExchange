6676b806946931c049fa96b387e0416fbc14a2c2
MatiMorinigo <mtsmorinigo@gmail.com>
2026-08-29 18:39:49 -0300

    feat(SCRUM-63): implementar consulta de clientes

--- clientes/views.py
+++ clientes/views.py
@@ -2,8 +2,27 @@ from rest_framework import generics
 
 from .models import Cliente
 from .serializers import ClienteSerializer
+from rest_framework.exceptions import NotFound
 
 
 class ClienteCreateView(generics.CreateAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+
+class ClienteListView(generics.ListAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+
+class ClienteDetailView(generics.RetrieveAPIView):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    lookup_field = "id_cliente"
+
+    def get_object(self):
+        try:
+            return Cliente.objects.get(
+                id_cliente=self.kwargs["id_cliente"]
+            )
+        except Cliente.DoesNotExist:
+            raise NotFound("Cliente no encontrado.")
